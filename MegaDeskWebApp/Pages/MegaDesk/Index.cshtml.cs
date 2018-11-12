5826fc1e0e825680ef465500360cca8a2b6d218d
Dan Ries <dan.sonso1@gmail.com>
2018-11-12 14:27:50 -0800

    Add files via upload

---
+++ MegaDeskWebApp/Pages/MegaDesk/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MegaDeskWebApp.Models;
+
+namespace MegaDeskWebApp.Pages.MegaDesk
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MegaDeskWebApp.Models.MegaDeskWebAppContext _context;
+
+        public IndexModel(MegaDeskWebApp.Models.MegaDeskWebAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<DeskQuote> DeskQuote { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            DeskQuote = await _context.DeskQuote.ToListAsync();
+        }
+    }
+}
